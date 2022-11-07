a4110d85911383c33a8daf343373ca79cb1e7478
Natawik-B <natawwwik37@gmail.com>
2022-11-07 19:48:17 +0300

    feat: add the third-solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const dirPath = path.join(__dirname, "secret-folder");
+
+function showInfo(file) {
+  fs.stat(path.join(dirPath, file.name),
+    (error, stats) => {
+      if (!error) {
+        const info = path.parse(file.name);
+        console.log(`${info.name} - ${info.ext.slice(1)} - ${stats.size} bytes`);
+      } else {
+        console.log(error);
+      }
+    })
+  };
+
+fs.readdir(dirPath, {withFileTypes: true},
+  (error, files) => {
+    if (error) {
+      console.log(error);
+    } else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          showInfo(file);
+        }
+      })
+    }
+  });
