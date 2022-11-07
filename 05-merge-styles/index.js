9d72f3ddb37966c2843b7b0d664b13b07f0a6fd9
Natawik-B <natawwwik37@gmail.com>
2022-11-08 00:28:00 +0300

    feat: add the fifth-solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+const stylesWay = path.join(__dirname, "styles");
+const projectDist = path.join(__dirname, "project-dist");
+
+fs.unlink(path.join(projectDist, "bundle.css"), (err) => {
+  if (err) {
+    console.log("No file");
+  };
+});
+
+fs.readdir(stylesWay, {withFileTypes: true}, (err, allFiles) => {
+  if (err) {
+    throw err;
+  } else {
+    allFiles.filter(file => file.isFile() && file.name.split(".").slice(-1) == "css")
+            .map(el => el.name)
+            .forEach(function(file) {
+              let readableStream = fs.createReadStream(path.join(stylesWay, file));
+              readableStream.on("data", data => {
+                fs.appendFile(path.join(projectDist, "bundle.css"), data, err => {
+                  if (err) {
+                    throw err;
+                  };
+                });
+              });
+            });
+  };
+});
